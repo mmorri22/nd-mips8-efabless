cbc7d3b66cc608e47c4ca4726c259f9dd56300a2
jguerra4 <94867838+jguerra4@users.noreply.github.com>
2023-12-09 15:02:41 -0500

    Update controller_test.v

--- verilog/rtl/controller_test.v
+++ verilog/rtl/controller_test.v
@@ -1,3 +1,18 @@
 module controller_test; 
 
   localparam integer HLT=0, SKZ=1, ADD=2, AND=3, XOR=4, LDA=5, STO=6, JMP=7;
+
+  reg [
+  reg [
+
+  wire [4:0] check_guess ; //wire for the check guess states 
+  wire        win; //wire for winning  
+  wire        lose; //wire for losing 
+
+  
+  controller controller_inst \
+
+
+    
+    
+  
